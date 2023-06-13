37d14c5549eb5a48dc70fe9a23c74bebe2670aef
Natalia Galvao <Natalia.Galvao.CTR@aiworldwide.com>
2023-06-13 01:25:13 -0300

    Change to worker pool factoring and fix checking_file

--- app/app.go
+++ app/app.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"runtime"
 	"strings"
-	"time"
 
 	"github.com/urfave/cli"
 )
@@ -56,7 +56,8 @@ func setup_to_search(c *cli.Context) {
 }
 
 func searching(path, content string) {
-	channel := make(chan string)
+	channel_files := make(chan os.DirEntry)
+	channel_results := make(chan string)
 
 	dir, err := os.Open(path)
 	if err != nil {
@@ -75,36 +76,47 @@ func searching(path, content string) {
 		log.Fatal(err)
 	}
 
-	go func() {
-		for _, file := range files {
-			if reg.MatchString(file.Name()) {
-				channel <- file.Name()
-				time.Sleep(time.Second * 1)
-			}
-		}
-	}()
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go searching_routine(path, content, reg, channel_files, channel_results)
+	}
 
-	go func() {
-		for _, file := range files {
-			if extension := strings.Split(file.Name(), "."); extension[len(extension)-1] == "txt" {
-				data, err := os.ReadFile(path + "/" + file.Name())
-				if err != nil {
-					log.Fatal(err)
-				}
-				found_content := strings.Split(string(data), " ")
-				for _, checking := range found_content {
-					if checking == content {
-						channel <- file.Name()
-						time.Sleep(time.Second * 2)
-					}
-				}
-			}
-		}
-		close(channel)
-	}()
+	for _, file := range files {
+		channel_files <- file
+	}
 
-	for found := range channel {
+	close(channel_files)
+
+	for found := range channel_results {
 		fmt.Println(found)
 	}
 
+}
+
+func searching_routine(path, content string, reg *regexp.Regexp, channel_files <-chan os.DirEntry, channel_results chan<- string) {
+	for file := range channel_files {
+		if file_found := checking_files(file, path, content, reg); len(file_found) > 0 {
+			channel_results <- file_found
+		}
+	}
+
+}
+
+func checking_files(file os.DirEntry, path, content string, reg *regexp.Regexp) string {
+	if reg.MatchString(file.Name()) {
+		return file.Name()
+	} else {
+		if extension := strings.Split(file.Name(), "."); extension[len(extension)-1] == "txt" {
+			data, err := os.ReadFile(path + "/" + file.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+			found_content := strings.Split(string(data), " ")
+			for _, checking := range found_content {
+				if checking == content {
+					return file.Name()
+				}
+			}
+		}
+	}
+	return ""
 }
